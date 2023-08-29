b959ab8c886b5356dd9b252548168a463fc8cb71
Giancarlo Castañeda <48701589+Giancarlo-Castaneda@users.noreply.github.com>
2023-08-29 11:27:05 -0500

    Revert "feat(networking): create networking layer"
    This reverts commit 8252a8a725e70c10e8c131d8ccd1f92703362085.

--- Ecommerce/Ecommerce/Networking/NetworkingProvider.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public protocol NetworkingProvider {
-
-    func sendRequest(endpoint: RequestType) async throws
-    func sendRequest<T: Decodable>(endpoint: RequestType, responseModel: T.Type) async throws -> T
-}
